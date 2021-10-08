62231666df0c28e496c5dd1cda0def6bd27cd5c1
uriyang <girlstoryk@gmail.com>
2021-10-09 00:58:38 +0900

    add createBoard Method [POST]

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 @Controller('boards')
@@ -8,5 +8,13 @@ export class BoardsController {
   @Get('/')
   getAllBoard(): Board[] {
     return this.boardService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardService.createBoard(title, description);
   }
 }
