3a2514376c873eb81227bbb417412f1ba52bd3c3
uriyang <girlstoryk@gmail.com>
2021-10-10 19:51:42 +0900

    add customPipe

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -12,6 +12,7 @@ import {
 import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 @Controller('boards')
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
@@ -40,7 +41,7 @@ export class BoardsController {
   @Patch('/:id/status')
   updateBoardStatus(
     @Param('id') id: string,
-    @Body('status') status: BoardStatus,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
   ): Board {
     return this.boardService.updateBoardStatus(id, status);
   }
